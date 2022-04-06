84a7a630e0d5d22b99f38678214b6e99fc9cd606
GregKovacs91 <36516107+GregKovacs91@users.noreply.github.com>
2022-04-06 10:33:21 +0200

    Delete SwitchCase.sql

--- SwitchCase.sql
+++
@@ -1,7 +0,0 @@
-SELECT OrderID, Quantity,
-CASE
-    WHEN Quantity > 30 THEN 'The quantity is greater than 30'
-    WHEN Quantity = 30 THEN 'The quantity is 30'
-    ELSE 'The quantity is under 30'
-END AS QuantityText
-FROM OrderDetails;
